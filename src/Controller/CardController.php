58d2e83af10e2608aa8e4d4b7adec3767850358c
simonking99 <sibo18@student.bth.se>
2025-04-09 23:01:25 +0200

    Added CardController with /card route

---
+++ src/Controller/CardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CardController extends AbstractController
+{
+    #[Route("/card", name: "card")]
+    public function card(): Response
+    {
+        return $this->render('card/card.html.twig');
+    }
+}
