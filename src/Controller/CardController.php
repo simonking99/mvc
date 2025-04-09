0653f83d721a408ab72bc1b871a1c370762aa564
simonking99 <sibo18@student.bth.se>
2025-04-10 00:30:40 +0200

    Added card/deck route to CardController to show deck of cards

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Card\DeckOfCards;
 
 class CardController extends AbstractController
 {
@@ -13,4 +15,21 @@ class CardController extends AbstractController
     {
         return $this->render('card/card.html.twig');
     }
+
+    #[Route("/card/deck", name: "card_deck")]
+    public function card_deck(SessionInterface $session): Response
+    {
+        $deck = $session->get('deck');
+    
+        if (!$deck) {
+            $deck = new DeckOfCards(true);
+            $session->set('deck', $deck);
+        }
+        $this->addFlash('message', 'The deck has been shown.');
+
+        return $this->render('card/card_deck.html.twig', [
+            'cards' => $deck->getCards()
+        ]);
+    }
+    
 }
