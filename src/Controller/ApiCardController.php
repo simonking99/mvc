d96e8cd9eb177d878745fcbbdfed700e033e7342
simonking99 <sibo18@student.bth.se>
2025-04-10 13:14:53 +0200

    Updated ApiCardController with new route

--- src/Controller/ApiCardController.php
+++ src/Controller/ApiCardController.php
@@ -5,10 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use App\Card\DeckOfCards;
 
 class ApiCardController extends AbstractController
 {
+    #[Route("/api", name: "api_landing")]
+    public function apiLanding(): Response
+    {
+        return $this->render('api/index.html.twig');
+    }
+
     #[Route("/api/deck", name: "api_deck", methods: ["GET"])]
     public function apiDeck(): JsonResponse
     {
@@ -23,6 +31,21 @@ class ApiCardController extends AbstractController
         return $this->json([
             'deck' => $data,
             'count' => count($data)
+        ]);
+    }
+
+    #[Route("/api/deck/shuffle", name: "api_deck_shuffle", methods: ["POST"])]
+    public function apiShuffle(SessionInterface $session): JsonResponse
+    {
+        $deck = new DeckOfCards(true);
+        $deck->shuffle();
+        $session->set('deck', $deck);
+        $cards = array_map(fn($card) => (string) $card, $deck->getCards());
+
+        return $this->json([
+            'deck' => $cards,
+            'count' => count($cards),
+            'status' => 'shuffled'
         ]);
     }
 }
