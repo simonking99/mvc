32c004ff156ee71fcbf81d833e2896a892a61632
simonking99 <sibo18@student.bth.se>
2025-04-14 12:10:26 +0200

    Added Psuedocode for the game class

---
+++ src/Game/Game.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Card;
+
+use App\Card\DeckOfCards;
+use App\Card\Card;
+use App\Card\CardHand;
+
+class Game
+{
+    /*
+     * Following functionality will be used:
+     * - Create a deck of cards and shuffle it
+     * - Create a hand for the player and the dealer
+     * - Deal cards to the player and the dealer
+     * - Sum the values of the cards in the player's/dealer's hand
+     * - Finalize the game and determine the winner
+     */
+}
