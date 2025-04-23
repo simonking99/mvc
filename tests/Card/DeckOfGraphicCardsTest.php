a2d75b65f8df6aaa67b33b99a3b84114c1834c50
simonking99 <sibo18@student.bth.se>
2025-04-23 10:18:52 +0200

    Construct object and verify that it is an instance of DeckOfGraphicCards

---
+++ tests/Card/DeckOfGraphicCardsTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests\Card;
+
+use PHPUnit\Framework\TestCase;
+use App\Card\DeckOfGraphicCards;
+
+class DeckOfGraphicCardsTest extends TestCase
+{
+    // Construct object and verify that it is an instance of DeckOfGraphicCards.
+    public function testCreateObject()
+    {
+        $obj = new DeckOfGraphicCards();
+        $this->assertInstanceOf(DeckOfGraphicCards::class, $obj);
+    }
+}
