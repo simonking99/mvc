aa842c32ee174109274582e1e4851fcabd4c7af9
simonking99 <sibo18@student.bth.se>
2025-04-23 10:02:44 +0200

    Created test for accessing card properties

---
+++ tests/Card/CardTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tests\Card;
+
+use PHPUnit\Framework\TestCase;
+use App\Card\Card;
+
+
+class CardTest extends TestCase
+{   
+    // Test to create a card and access its properties
+    public function testCreateAndAccess()
+    {
+        $card = new Card("♠", "A");
+
+        $this->assertInstanceOf(Card::class, $card);
+        $this->assertEquals("♠", $card->getSuit());
+        $this->assertEquals("A", $card->getValue());
+    }
+}
