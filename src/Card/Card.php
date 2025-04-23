18de9d683973de09be6d94ab718650e06a172d1a
simonking99 <sibo18@student.bth.se>
2025-04-23 11:53:44 +0200

    Updated comments with php dockblock

--- src/Card/Card.php
+++ src/Card/Card.php
@@ -2,32 +2,50 @@
 
 namespace App\Card;
 
+/**
+ * A card with a suit and value.
+ */
 class Card
 {
-    protected string $suit;
-    protected string $value;
+    protected string $suit;  // The suit of the card.
+    protected string $value; // The value of the card.
 
+    /**
+     * Create a card with a suit and value.
+     */
     public function __construct(string $suit, string $value)
     {
         $this->suit = $suit;
         $this->value = $value;
     }
 
+    /**
+     * Get the suit.
+     */
     public function getSuit(): string
     {
         return $this->suit;
     }
 
+    /**
+     * Get the value.
+     */
     public function getValue(): string
     {
         return $this->value;
     }
 
+    /**
+     * Set a new value.
+     */
     public function setValue(string $value): void
     {
         $this->value = $value;
     }
 
+    /**
+     * Get the card as a string.
+     */
     public function __toString(): string
     {
         return "{$this->value}{$this->suit}";
