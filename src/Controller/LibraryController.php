aaa64c2d907e4c1ce9f9b1d8750a2866e1aab06a
simonking99 <sibo18@student.bth.se>
2025-04-24 15:30:10 +0200

    New route to init database with 3 books

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Entity\Product;
+use App\Entity\Book;
 use App\Repository\ProductRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -15,4 +15,31 @@ final class LibraryController extends AbstractController
     public function index(): Response {
         return $this->render('library/index.html.twig');
     }
+
+    #[Route('/library/init', name: 'library_init')]
+    public function initLibrary(ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+    
+        $books = [
+            ['title' => '1984', 'isbn' => '9780451524935', 'author' => 'George Orwell', 'image' => '1984.jpg'],
+            ['title' => 'Brave New World', 'isbn' => '9780060850524', 'author' => 'Aldous Huxley', 'image' => 'brave_new_world.jpg'],
+            ['title' => 'Fahrenheit 451', 'isbn' => '9781451673319', 'author' => 'Ray Bradbury', 'image' => 'fahrenheit_451.jpg'],
+        ];
+    
+        foreach ($books as $data) {
+            $book = new Book();
+            $book->setTitle($data['title']);
+            $book->setIsbn($data['isbn']);
+            $book->setAuthor($data['author']);
+            $book->setImage($data['image']);
+            $entityManager->persist($book);
+        }
+    
+        $entityManager->flush();
+    
+        return $this->render('library/init.html.twig', [
+            'bookCount' => count($books),
+        ]);
+    }    
 }
