3342061e6ca3db58bd5e77b6400bcf85c7060d51
simonking99 <sibo18@student.bth.se>
2025-05-10 21:46:02 +0200

    Created getdealer function for Blackjack class

--- src/Game/Blackjack.php
+++ src/Game/Blackjack.php
@@ -4,5 +4,27 @@ namespace App\Game;
 
 class Blackjack
 {
+    /** @var Game[] */
+    private array $games = [];
 
+    private Game $dealerGame;
+
+    /** @var Player[] */
+    private array $players = [];
+
+    public function __construct(array $players)
+    {
+        foreach ($players as $player) {
+            $this->players[] = $player;
+            $this->games[] = new Game();
+        }
+
+        $this->dealerGame = new Game();
+        $this->dealerGame->setAsDealer();
+    }
+
+    public function getDealer(): Game
+    {
+        return $this->dealerGame;
+    }
 }
