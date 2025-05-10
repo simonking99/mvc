39aa106fd6e6008c988f4b8b8869e6eab944c61c
simonking99 <sibo18@student.bth.se>
2025-05-10 21:39:03 +0200

    Created getName function for Player class

--- src/Game/Player.php
+++ src/Game/Player.php
@@ -4,4 +4,19 @@ namespace App\Game;
 
 class Player
 {
+    private string $name;
+    private int $bank;
+    private int $bet;
+
+    public function __construct(string $name, int $initialBank = 1000)
+    {
+        $this->name = $name;
+        $this->bank = $initialBank;
+        $this->bet = 0;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
 }
