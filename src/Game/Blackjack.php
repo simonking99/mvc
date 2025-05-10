dda5cee763d83079b0f24ee3a349470734f412e9
simonking99 <sibo18@student.bth.se>
2025-05-10 21:48:41 +0200

    Created finishRound function for Blackjack class

--- src/Game/Blackjack.php
+++ src/Game/Blackjack.php
@@ -63,4 +63,46 @@ class Blackjack
         }
         $this->dealerGame->startRound();
     }
+
+    public function finishRound(): void
+    {
+        $this->dealerGame->dealerTurn();
+        $dealerHand = $this->dealerGame->getDealerHand();
+        $calculator = new ScoreCalculator();
+
+        foreach ($this->games as $index => $game) {
+            $player = $this->players[$index];
+            $playerScore = $calculator->calculate($game->getPlayerHand());
+            $dealerScore = $calculator->calculate($dealerHand);
+            $blackjack = ($playerScore === 21 && $game->getPlayerHand()->getNumberOfCards() === 2);
+
+            if ($playerScore > 21) {
+                $game->setWinner("Dealer Wins");
+                $player->applyLoss();
+            } elseif ($dealerScore > 21) {
+                $game->setWinner("Player Wins");
+                $player->applyWin($blackjack);
+            } elseif ($playerScore > $dealerScore) {
+                $game->setWinner("Player Wins");
+                $player->applyWin($blackjack);
+            } elseif ($playerScore < $dealerScore) {
+                $game->setWinner("Dealer Wins");
+                $player->applyLoss();
+            } else {
+                $game->setWinner("Draw");
+            }
+
+            $player->resetBet();
+        }
+
+        foreach ($this->players as $i => $player) {
+            if ($player->isBankrupt()) {
+                unset($this->players[$i]);
+                unset($this->games[$i]);
+            }
+        }
+
+        $this->players = array_values($this->players);
+        $this->games = array_values($this->games);
+    }
 }
